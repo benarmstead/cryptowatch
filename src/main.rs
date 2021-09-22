1307ddcab8842733e75bfbba80390ce3826b7d80
Ben Armstead <benarmstead@protonmail.com>
2021-09-22 10:43:01 +0100

    Get crypto prices from binance

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use binance::api::*;
+use binance::market::*;
+
 fn main() {
-    println!("Hello, world!");
+    let trade = "XMRUSDT".to_string();
+    let market: Market = Binance::new(None, None);
+    loop_print_price(market, trade);
+}
+
+fn loop_print_price(market: Market, trade: String) {
+    println!("{}", get_price(market, trade));
+}
+
+fn get_price(market: Market, trade: String) -> String {
+    match market.get_price(trade) {
+        Ok(answer) => return answer.price.to_string(),
+        Err(e) => return e.to_string(),
+    }
+}
+
+fn _get_data(market: Market) {
+    match market.get_24h_price_stats("XMRUSDT") {
+        Ok(answer) => println!("{:?}", answer),
+        Err(e) => println!("Error: {:?}", e),
+    }
 }
