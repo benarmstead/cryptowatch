c470f3123e23fd48b0d1cf3f18fcee4ed1368e68
Ben Armstead <benarmstead@protonmail.com>
2021-09-29 22:24:55 +0100

    Intial commit

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,51 @@
 use binance::api::*;
 use binance::market::*;
+use std::{thread, time};
 
 fn main() {
-    let trade = "XMRUSDT".to_string();
+    let trade_codes = vec![
+        "XMR/USDT".to_string(),
+        "BTC/USDT".to_string(),
+        "ETH/USDT".to_string(),
+    ];
     let market: Market = Binance::new(None, None);
-    loop_print_price(market, trade);
+    loop_print_price(market, trade_codes);
 }
 
-fn loop_print_price(market: Market, trade: String) {
-    println!("{}", get_price(market, trade));
-}
+fn loop_print_price(market: Market, trade_codes: Vec<String>) {
+    let trade_codes_len = trade_codes.len();
 
-fn get_price(market: Market, trade: String) -> String {
-    match market.get_price(trade) {
-        Ok(answer) => return answer.price.to_string(),
-        Err(e) => return e.to_string(),
+    let mut price: f64;
+    let mut prices: Vec<String> = vec!["".to_string(); trade_codes_len];
+
+    loop {
+        let final_length = prices.len().saturating_sub(trade_codes_len);
+        prices.truncate(final_length);
+
+        for i in &trade_codes {
+            price = get_price(&market, &i.replace("/", ""));
+            prices.push(price.to_string());
+        }
+
+        clearscreen::clear().expect("failed to clear screen");
+
+        for i in 0..trade_codes_len {
+            println!("{}: ${}", trade_codes[i], prices[i]);
+        }
+
+        sleep(5);
     }
+}
+
+fn get_price(market: &Market, trade_code: &str) -> f64 {
+    match market.get_price(trade_code) {
+        Ok(answer) => answer.price,
+        Err(_) => -0.1,
+    }
+}
+
+pub fn sleep(timer: u64) {
+    thread::sleep(time::Duration::from_secs(timer));
 }
 
 fn _get_data(market: Market) {
